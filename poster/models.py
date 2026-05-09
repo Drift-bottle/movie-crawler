e692269e9e82b4d9220ba3c748b17336150784cf
Drift-bottle <dh070307@163.com>
2026-05-09 17:43:40 +0800

    删除 to_dict 方法

--- poster/models.py
+++ poster/models.py
@@ -1,10 +1,7 @@
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
 
 @dataclass
 class PosterUrl:
     """电影海报url数据"""
     title: str # 电影标题
     poster_url: str # 海报url
-
-    def to_dict(self):
-        return asdict(self)
